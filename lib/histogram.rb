ba19c1322b1cb1fdbe6b60c796ad952811608644
John Prince <jtprince@gmail.com>
2009-11-24 18:19:10 -0700

    in progress on num bins calculations

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -1,28 +1,100 @@
 
 begin
   require 'narray'
-rescue
-  class NArray
+rescue loaderror
+  class narray
   end
 end
 
-module Histogram
+module histogram
 
-  # Returns (min, max)
-  def min_max
-    mn = self[0]
-    mx = self[0]
-    self.each do |val|
+  # returns (min, max)
+  def self.min_max(obj)
+    mn = obj[0]
+    mx = obj[0]
+    obj.each do |val|
       if val < mn then mn = val end
       if val > mx then mx = val end
     end
     [mn, mx]
   end
 
+  # returns (mean, standard_dev)
+  # if size == 0 returns [nil, nil]
+  def self.sample_stats(obj)
+    _len = obj.size
+    return [nil, nil] if _len == 0
+    _sum = 0.0 ; _sum_sq = 0.0
+    obj.each do |val|
+      _sum += val
+      _sum_sq += val * val
+    end
+    std_dev = _sum_sq - ((_sum * _sum)/_len)
+    std_dev /= ( _len > 1 ? _len-1 : 1 )
+    [_sum.to_f/_len, math.sqrt(std_dev)]
+  end
+
+  def self.iqrange(obj)
+    srted = obj.sort
+    sz = srted.size
+    if sz % 2 == 0
+      median_index_hi = sz / 2
+      median_index_lo = sz / 2 - 1
+      dist = sz / 2 
+      fq = srted[median_index_hi + dist]
+      tq = srted[median_index_lo - dist]
+    else
+      median_index = sz / 2
+      dist = (median_index + 1) / 2
+      fq = srted[median_index - dist]
+      tq = srted[median_index + dist]
+    end
+    (tq - fq).to_f
+  end
+
+  # returns(integer) takes :scott|:sturges|:fd|:middle
+  #
+  # middle is the median between the other three values
+  #
+  # inspired by {Richard Cotton's matlab
+  # implementation}[http://www.mathworks.com/matlabcentral/fileexchange/21033-calculate-number-of-bins-for-histogram]
+  # and the {histogram page on
+  # wikipedia}[http://en.wikipedia.org/wiki/Histogram]
+  def number_bins(methd=:fd)
+    if methd == :middle
+      [:scott, :sturges, :fd].map {|v| number_bins(v) }.sort[1]
+    else
+      range = (self.max - self.min).to_f
+      nbins = 
+        case methd
+        when :scott
+          (mean, stddev) = Histogram.sample_stats(self) 
+          range / ( 3.5*stddev*(ar.size**(-1.0/3)) )
+        when :sturges
+          (Math::log(self.size)/Math::log(2)) + 1
+        when :fd
+          range / ( 2*Histogram.iqrange(self)*ar.size**(-1.0/3) ) 
+        end
+      nbins = 1 if num <= 0
+      nbins.ceil.to_i
+    end
+  end
+
   # Returns [bins, freqs]
   #
-  #     bins = Number of bins to use or an Array/NArray specifying them
-  #     tp   = :avg || :min  (:avg default) how to bin the values
+  # Options:
+  #
+  #     :bins => :fd       Freedman-Diaconis range/(2*iqrange *n^(-1/3)) (default)
+  #              :scott    Scott's method    range/(3.5σ * n^(-1/3))
+  #              :sturges  Sturges' method   log_2(n) + 1
+  #              :middle   the median between three above
+  #              <Integer> give the number of bins
+  #              <Array>   specify the bins themselves
+  #
+  #     :tp   => :avg      boundary is the avg between bins (default)
+  #              :min      bins specify the minima for binning
+  #
+  #     :bin_width => <float> width of a bin (overrides :bins)
   #
   # Examples 
   #
@@ -63,7 +135,13 @@ module Histogram
   #   to histogram and the next array is the y values (or intensities) to be
   #   applied in the histogram. (checks for !first_value.is_a?(Numeric))
   # * Return value
-  def histogram(bins=5, tp=:avg, *other_sets)
+  def histogram(opts={})
+    DEFAULT_OPTS = {
+      :bins => nil,
+      :tp => :avg,
+      :other_sets => []
+    }
+
     make_freqs = lambda do |obj, len|
       if obj.is_a?(Array)
         Array.new(len, 0.0)
@@ -149,10 +227,10 @@ module Histogram
       # Create the scaling factor
 
       (xvals, yvals) = have_frac_freqs ? [self[0], self[1]] : [self, nil]
-      _min, _max = xvals.min_max
+      _min, _max = Histogram.min_max(xvals)
       other_sets.each do |vec|
         (xvals, yvals) = have_frac_freqs ? [vec[0], vec[1]] : [vec, nil]
-        v_min, v_max = xvals.min_max
+        v_min, v_max = Histogram.min_max(xvals)
         if v_min < _min ; _min = v_min end 
         if v_max > _max ; _max = v_max end 
       end
--- spec/histogram_spec.rb
+++ spec/histogram_spec.rb
@@ -66,6 +66,14 @@ TestArrays = [[0,1,2,3,4,5,6,7,8,9,10], [0, 1, 1.5, 2.0, 5.0, 6.0, 7, 8, 9, 9],
   [-1, 0, 1, 1.5, 2.0, 5.0, 6.0, 7, 8, 9, 9, 10], [1, 1, 2, 2, 3, 3, 4, 4, 4],
   [2, 2, 2, 2, 2, 4]]
 
+describe 'calculating bins' do
+  it 'calculates :sturges, :scott, or :fd' do
+         
+  end
+end
+
+
+
 require 'histogram/array'
 describe 'histogramming an Array' do
   before do
